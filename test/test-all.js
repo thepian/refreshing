dfe716907ca8d218a338c159d44a6dd733b562eb
Thepian <henrik@thepia.com>
2013-03-16 14:44:47 +0100

    nodeunit

--- test/test-all.js
+++ test/test-all.js
@@ -1,5 +1,12 @@
-#!/usr/bin/env node
+#!/usr/bin/env nodeunit
 
+exports.testConcatenate = function(test) {
+	test.expect(1);
+	test.ok(true,"hmm strange");
+	test.done();
+};
+
+/*
 var base = __dirname;
 //if (process.argv.length > 2) base = require('path').join(__dirname,process.argv[2]);
 
@@ -10,3 +17,4 @@ require("../lib/refreshing/index").watchTrees(__dirname,base);
 base = require('path').join(__dirname,"sub-out");
 
 require("../lib/refreshing/index").watchTrees(__dirname,base);
+*/
